556dbc7b2c20a5e7d9bc04a325d0da4fd7d2869e
yanniel kessel <yanniel@danngos.com>
2022-07-24 22:54:45 -0400

    Agregado la base del panel de administracion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CaracteristicasComponent } from './privado/caracteristicas/caracteristicas.component';
+import { DocumentosComponent } from './privado/documentos/documentos.component';
+import { IdiomasComponent } from './privado/idiomas/idiomas.component';
+import { LoginComponent } from './privado/login/login.component';
+import { MetodosComponent } from './privado/metodos/metodos.component';
+import { SeccionesComponent } from './privado/secciones/secciones.component';
+import { ServiciosComponent } from './privado/servicios/servicios.component';
+import { SidebarComponent } from './privado/sidebar/sidebar.component';
+import { UsuariosComponent } from './privado/usuarios/usuarios.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  {
+    path: 'sidebar',
+    component: SidebarComponent,
+    children: [
+      { path: 'secciones', component: SeccionesComponent },
+      { path: 'metodos', component: MetodosComponent },
+      { path: 'documentos', component: DocumentosComponent },
+      { path: 'caracteristicas', component: CaracteristicasComponent },
+      { path: 'servicios', component: ServiciosComponent },
+      { path: 'usuarios', component: UsuariosComponent },
+      { path: 'idiomas', component: IdiomasComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SidebarComponent } from './privado/sidebar/sidebar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { SeccionesComponent } from './privado/secciones/secciones.component';
+import { CaracteristicasComponent } from './privado/caracteristicas/caracteristicas.component';
+import { MetodosComponent } from './privado/metodos/metodos.component';
+import { ServiciosComponent } from './privado/servicios/servicios.component';
+import { DocumentosComponent } from './privado/documentos/documentos.component';
+import { UsuariosComponent } from './privado/usuarios/usuarios.component';
+import { IdiomasComponent } from './privado/idiomas/idiomas.component';
+import { LoginComponent } from './privado/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    SeccionesComponent,
+    CaracteristicasComponent,
+    MetodosComponent,
+    ServiciosComponent,
+    DocumentosComponent,
+    UsuariosComponent,
+    IdiomasComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
