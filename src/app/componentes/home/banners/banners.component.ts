702f86321248625b9ae898388b4a67fbff37cb83
yanniel kessel <yanniel@danngos.com>
2022-09-10 12:16:05 -0400

    Fix reponsive of banner, carrusel and nav

--- src/app/componentes/home/banners/banners.component.ts
+++ src/app/componentes/home/banners/banners.component.ts
@@ -17,67 +17,42 @@ import { first, take } from 'rxjs/operators';
   templateUrl: './banners.component.html',
   styleUrls: ['./banners.component.scss'],
   encapsulation: ViewEncapsulation.None,
-  animations: [
-    trigger('enterLeave', [
-      state('enter', style({
-        position: 'absolute',
-          top: 0,
-          // left: 0,
-          right: 0,
-          width: '100%'
-      })),
+  // animations: [
+  //   trigger('enterLeave', [
+  //     state('enter', style({
+  //       // position: 'absolute',
+  //         // top: 0,
+  //         // left: 0,
+  //         'margin-right': 0,
+  //         width: '100%'
+  //     })),
 
-      state('leave', style({
-        position: 'absolute',
-          top: 0,
-          right: 0,
-          // left: 0,
-          width: '100%'
-      })),
+  //     state('leave', style({
+  //       // position: 'absolute',
+  //         // top: 0,
+  //         right: 0,
+  //         // left: 0,
+  //         width: '100%'
+  //     })),
 
-      transition('enter => leave', [
-        style({ position: 'relative' }),
-        animate('1000ms ease-out', style({ right: '100%', opacity: 0 }))
-      ]),
-      transition('leave => enter', [
-        style({ position: 'relative' }),
-        style({ right: '-100%' }),
-        animate('1000ms ease-out', style({ right: '0%' }))
-      ]),
-
-      // transition('enter <=> leave', [
-      //   style({ position: 'relative' }),
-      //   query(':enter, :leave', [
-      //     style({
-      //       position: 'absolute',
-      //       top: 0,
-      //       left: 0,
-      //       width: '100%'
-      //     })
-      //   ]),
-      //   query(':enter', [
-      //     style({ left: '-100%' })
-      //   ]),
-      //   query(':leave', animateChild()),
-      //   group([
-      //     query(':leave', [
-      //       animate('200ms ease-out', style({ left: '100%', opacity: 0 }))
-      //     ]),
-      //     query(':enter', [
-      //       animate('300ms ease-out', style({ left: '0%' }))
-      //     ]),
-      //     query('@*', animateChild())
-      //   ]),
-      // ])
-    ])
-  ]
+  //     // transition('enter => leave', [
+  //     //   style({ position: 'relative' }),
+  //     //   animate('1000ms ease-out', style({ right: '100%', opacity: 0 }))
+  //     // ]),
+  //     transition('leave => enter', [
+  //       // style({ position: 'relative' }),
+  //       style({ 'margin-right': '-100%' }),
+  //       animate('1000ms ease-out', style({ 'margin-right': '0%' }))
+  //     ]),
+  //   ])
+  // ]
 })
 export class BannersComponent implements OnInit, AfterViewInit, OnDestroy {
   lang: string = "";
   hideId: number = 0;
   total: number = 0;
   idInterval!: any;
-  time: number = 3000;
+  time: number = 5000;
   banners$ = this.store.select(selectBanners);
   lang$ = this.store.select(selectLang).subscribe((l) => (this.lang = l));
   totalBanners = this.banners$.subscribe((a) => this.total = a.length)
@@ -109,7 +84,7 @@ export class BannersComponent implements OnInit, AfterViewInit, OnDestroy {
     else if(this.hideId === this.total) {
       clearInterval(this.idInterval);
       this.hideId = 0;
-      this.idInterval = setInterval(this.changeBanner, 3000);
+      this.idInterval = setInterval(this.changeBanner, 5000);
     }
   }
 }
