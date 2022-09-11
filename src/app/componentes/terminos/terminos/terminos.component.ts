9d47236efb831256f7411e3c235c5eda3d6026b8
yanniel kessel <yanniel@danngos.com>
2022-09-11 14:08:15 -0400

    Fix some design errors and add terminos page

--- src/app/componentes/terminos/terminos/terminos.component.ts
+++ src/app/componentes/terminos/terminos/terminos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectSecciones } from 'src/app/store/secciones/secciones.selectors';
+import { selectLang } from 'src/app/store/system/system.selectors';
 
 @Component({
   selector: 'bipay-terminos',
@@ -6,8 +9,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./terminos.component.scss']
 })
 export class TerminosComponent implements OnInit {
+  lang: string = '';
+  secciones$ = this.store.select(selectSecciones);
+  lang$ = this.store.select(selectLang).subscribe((l) => (this.lang = l));
 
-  constructor() { }
+  constructor(private store: Store) { }
 
   ngOnInit(): void {
   }
