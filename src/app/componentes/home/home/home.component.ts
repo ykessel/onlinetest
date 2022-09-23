2a4f851feea73b396f5c101cb751cca4487e6741
yanniel kessel <yanniel@danngos.com>
2022-09-23 14:25:05 -0400

    Added carrusel to pagos and added animation to download button

--- src/app/componentes/home/home/home.component.ts
+++ src/app/componentes/home/home/home.component.ts
@@ -84,7 +84,7 @@ export class HomeComponent implements OnInit {
         items: 1,
       },
       760: {
-        items: 1,
+        items: 2,
       },
       1024: {
         items: 3,
@@ -118,7 +118,7 @@ export class HomeComponent implements OnInit {
         items: 1,
       },
       760: {
-        items: 1,
+        items: 3,
       },
       1024: {
         items: 4,
@@ -138,9 +138,9 @@ export class HomeComponent implements OnInit {
 
   customOptions3: OwlOptions = {
     loop: false,
-    mouseDrag: false,
+    mouseDrag: true,
     touchDrag: true,
-    pullDrag: false,
+    pullDrag: true,
     dots: false,
     navSpeed: 600,
     navText: ["", ""],
@@ -152,19 +152,19 @@ export class HomeComponent implements OnInit {
         items: 3,
       },
       760: {
-        items: 3,
+        items: 5,
       },
       1024: {
-        items: 4,
+        items: 6,
       },
       1366: {
-        items: 5,
+        items: 6,
       },
       1920: {
         items: 6,
       },
       2000: {
-        items: 7,
+        items: 6,
       },
     },
     nav: false,
