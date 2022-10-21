439ae055bc189e7621eb7e31a2a0a6e51a18022f
yanniel kessel <yanniel@danngos.com>
2022-10-21 03:21:56 -0400

    Added configuration cookies modal

--- src/app/componentes/shared/cookies/cookies.component.ts
+++ src/app/componentes/shared/cookies/cookies.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { faXmarkCircle } from '@fortawesome/free-solid-svg-icons';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import { Store } from '@ngrx/store';
+import { selectLang } from 'src/app/store/system/system.selectors';
 
 @Component({
   selector: 'bipay-cookies',
@@ -7,16 +14,48 @@ import { faXmarkCircle } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./cookies.component.scss']
 })
 export class CookiesComponent implements OnInit {
-  hide: boolean = false;
+  hide: boolean = localStorage.getItem('showCookie') == 'false' ? true : false;
   faXmarkCircle = faXmarkCircle;
 
-  constructor() { }
+  constructor(private store: Store,  public dialog: MatDialog) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  hideCookie() {
+    localStorage.setItem('showCookie', JSON.stringify(false));
+    this.hide = true;
   }
+
+  openDialog() {
+    this.hideCookie()
+    this.dialog.open(ConfigCookie, {
+      panelClass: "cookie-modal"
+    });
+  }
+
+}
+
+@Component({
+  selector: 'config-cookie',
+  templateUrl: 'config-cookie.html',
+  styleUrls: ['./config-cookie.scss'],
+})
+export class ConfigCookie {
+  lang: string = '';
+  lang$ = this.store.select(selectLang).subscribe((l) => (this.lang = l));
+  faXmarkCircle = faXmarkCircle;
+  hide: boolean = false;
+
+  constructor(
+    public dialogRef: MatDialogRef<ConfigCookie>,
+    private store: Store
+  ) {}
 
   hideCookie() {
     this.hide = true;
   }
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
